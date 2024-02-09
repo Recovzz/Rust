8d6de3db76a8ea6cddb0bfa94d892b8cc34c9543
Recovz <93311330+Recovzz@users.noreply.github.com>
2024-02-09 10:51:32 +0100

    Update structs3.rs

--- exercices/07_structs/structs3.rs
+++ exercices/07_structs/structs3.rs
@@ -1,14 +1,4 @@
-// structs3.rs
-//
-// Structs contain data, but can also have logic. In this exercise we have
-// defined the Package struct and we want to test some logic attached to it.
-// Make the code compile and the tests pass!
-//
-// Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// La structure `Package` est définie avec des champs de données et des méthodes associées.
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -17,11 +7,11 @@ struct Package {
 }
 
 impl Package {
+    // La méthode `new` crée une nouvelle instance de `Package`.
+    // Si le poids est inférieur à 10, elle panique avec un message d'erreur.
     fn new(sender_country: String, recipient_country: String, weight_in_grams: u32) -> Package {
         if weight_in_grams < 10 {
-            // This is not how you should handle errors in Rust,
-            // but we will learn about error handling later.
-            panic!("Can not ship a package with weight below 10 grams.")
+            panic!("Cannot ship a package with weight below 10 grams.")
         } else {
             Package {
                 sender_country,
@@ -31,12 +21,14 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    // La méthode `is_international` vérifie si l'envoi est international.
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
+    // La méthode `get_fees` calcule les frais de transport en fonction du poids du colis.
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
