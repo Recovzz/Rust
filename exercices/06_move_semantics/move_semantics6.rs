c537cb27486222510ac3d03347d13bb7805a7d37
Recovz <93311330+Recovzz@users.noreply.github.com>
2024-02-09 09:35:32 +0100

    Update move_semantics6.rs

--- exercices/06_move_semantics/move_semantics6.rs
+++ exercices/06_move_semantics/move_semantics6.rs
@@ -1,28 +1,22 @@
-// move_semantics6.rs
-//
-// You can't change anything except adding or removing references.
-//
-// Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
-// for a hint.
-
-// I AM NOT DONE
+// J'ai modifié la fonction get_char pour qu'elle prenne en ref la string j'ai fais pareil avec la fonction string_uppercase. 
+// J'ai aussi retiré le mut de data car on en a pas besoin puisque nous ne modifions pas la ref elle même mais + la valeur qu'elle pointe
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
     string_uppercase(&data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(data: &String) {
+    let uppercase_data = data.to_uppercase();
 
-    println!("{}", data);
+    println!("{}", uppercase_data);
 }
