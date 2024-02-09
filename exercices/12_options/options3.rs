260e049cfadf0d469dbe297ac63006661203356f
Recovz <93311330+Recovzz@users.noreply.github.com>
2024-02-09 09:53:57 +0100

    Update options3.rs

--- exercices/12_options/options3.rs
+++ exercices/12_options/options3.rs
@@ -1,10 +1,4 @@
-// options3.rs
-//
-// Execute `rustlings hint options3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// 
 struct Point {
     x: i32,
     y: i32,
@@ -14,8 +8,8 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y), // Utilisation de `ref` pour créer une référence au lieu de déplacer la valeur
         _ => panic!("no match!"),
-    }
+    } 
     y; // Fix without deleting this line.
 }
