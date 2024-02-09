cd1a7ed7fb146b44417a06690bc560dfae82ef0e
Recovz <93311330+Recovzz@users.noreply.github.com>
2024-02-09 10:45:05 +0100

    Update quiz3.rs

--- exercices/23_conversions/quiz3.rs
+++ exercices/23_conversions/quiz3.rs
@@ -1,33 +1,66 @@
-// quiz3.rs
-//
-// This quiz tests:
-// - Generics
-// - Traits
-//
-// An imaginary magical school has a new report card generation system written
-// in Rust! Currently the system only supports creating report cards where the
-// student's grade is represented numerically (e.g. 1.0 -> 5.5). However, the
-// school also issues alphabetical grades (A+ -> F-) and needs to be able to
-// print both types of report card!
-//
-// Make the necessary code changes in the struct ReportCard and the impl block
-// to support alphabetical report cards. Change the Grade in the second test to
-// "A+" to show that your changes allow alphabetical grades.
-//
-// Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
+// Je vais utiliser une énumération pour représenter les notes alphabétiques et numériques.
+// Ensuite, je vais implémenter un trait qui convertit les notes numériques en notes alphabétiques et vice versa.
 
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+// J'implémente le trait GradeConverter qui aura une méthode convert_grade.
+trait GradeConverter {
+    fn convert_grade(&self) -> String;
+}
+
+// J'implémente le trait pour les types f32 et String pour convertir les grades numériques en grades alphabétiques et vice versa.
+impl GradeConverter for f32 {
+    fn convert_grade(&self) -> String {
+        if *self >= 4.5 {
+            "A+".to_string()
+        } else if *self >= 4.0 {
+            "A".to_string()
+        } else if *self >= 3.5 {
+            "B+".to_string()
+        } else if *self >= 3.0 {
+            "B".to_string()
+        } else if *self >= 2.5 {
+            "C+".to_string()
+        } else if *self >= 2.0 {
+            "C".to_string()
+        } else if *self >= 1.5 {
+            "D+".to_string()
+        } else if *self >= 1.0 {
+            "D".to_string()
+        } else {
+            "F".to_string()
+        }
+    }
+}
+
+impl GradeConverter for String {
+    fn convert_grade(&self) -> String {
+        match self.as_str() {
+            "A+" => "4.5".to_string(),
+            "A" => "4.0".to_string(),
+            "B+" => "3.5".to_string(),
+            "B" => "3.0".to_string(),
+            "C+" => "2.5".to_string(),
+            "C" => "2.0".to_string(),
+            "D+" => "1.5".to_string(),
+            "D" => "1.0".to_string(),
+            _ => "0.0".to_string(),
+        }
+    }
+}
+
+// Implémentation de la méthode print pour ReportCard en utilisant le trait GradeConverter.
+impl<T: GradeConverter> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        let grade = self.grade.convert_grade();
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, grade
+        )
     }
 }
 
@@ -50,9 +83,9 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
+        // Je passe une chaîne de caractères représentant la note alphabétique.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
