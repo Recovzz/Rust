27f251ae6d2ba871ec2d1bde7e6faf291d9276a2
Recovz <93311330+Recovzz@users.noreply.github.com>
2024-02-09 10:37:24 +0100

    Update iterators5.rs

--- exercices/18_iterators/iterators5.rs
+++ exercices/18_iterators/iterators5.rs
@@ -1,17 +1,4 @@
-// iterators5.rs
 //
-// Let's define a simple model to track Rustlings exercise progress. Progress
-// will be modelled using a hash map. The name of the exercise is the key and
-// the progress is the value. Two counting functions were created to count the
-// number of exercises with a given progress. Recreate this counting
-// functionality using iterators. Try not to use imperative loops (for, while).
-// Only the two iterator methods (count_iterator and count_collection_iterator)
-// need to be modified.
-//
-// Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -32,10 +19,10 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
     count
 }
 
+// Modifier cette fonction pour utiliser des itérateurs
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
-    // map is a hashmap with String keys and Progress values.
-    // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    // Utiliser un itérateur pour filtrer les valeurs égales à `value`, puis compter les éléments filtrés
+    map.values().filter(|&val| *val == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -50,11 +37,10 @@ fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progres
     count
 }
 
+// Modifier cette fonction pour utiliser des itérateurs
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
-    // collection is a slice of hashmaps.
-    // collection = [{ "variables1": Complete, "from_str": None, ... },
-    //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    // Utiliser un itérateur pour aplatir la collection et compter les valeurs égales à `value`
+    collection.iter().flat_map(|map| map.values()).filter(|&&val| val == value).count()
 }
 
 #[cfg(test)]
