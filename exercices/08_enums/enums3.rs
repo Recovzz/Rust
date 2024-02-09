14f0dfa6830584138a6f3763cf9917e15644350e
Recovz <93311330+Recovzz@users.noreply.github.com>
2024-02-09 10:58:12 +0100

    Update enums3.rs

--- exercices/08_enums/enums3.rs
+++ exercices/08_enums/enums3.rs
@@ -1,14 +1,10 @@
-// enums3.rs
-//
-// Address all the TODOs to make the tests pass!
-//
-// Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// Nous définissons une énumération `Message` pour représenter les différents types de messages
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    // Chaque variante correspond à un type de message spécifique avec ses données associées
+    ChangeColor(u8, u8, u8), // Pour changer la couleur, trois valeurs RGB
+    Echo(String),            // Pour l'écho, une chaîne de caractères
+    Move(Point),             // Pour le mouvement, une structure `Point`
+    Quit,                    
 }
 
 struct Point {
@@ -24,26 +20,35 @@ struct State {
 }
 
 impl State {
+    // Changement de la couleur
     fn change_color(&mut self, color: (u8, u8, u8)) {
         self.color = color;
     }
 
+    
     fn quit(&mut self) {
         self.quit = true;
     }
 
+    
     fn echo(&mut self, s: String) {
-        self.message = s
+        self.message = s;
     }
 
+   
     fn move_position(&mut self, p: Point) {
         self.position = p;
     }
 
+ 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
-        // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
-        // fn function((t, u, p, l, e))
+        // Expression `match` pour traiter les différentes variantes de message
+        match message {
+            Message::ChangeColor(color1, color2, color3) => self.change_color((color1, color2, color3)),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
@@ -59,11 +64,13 @@ mod tests {
             color: (0, 0, 0),
             message: "hello world".to_string(),
         };
+        
         state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("Hello world!")));
         state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
 
+      
         assert_eq!(state.color, (255, 0, 255));
         assert_eq!(state.position.x, 10);
         assert_eq!(state.position.y, 15);
