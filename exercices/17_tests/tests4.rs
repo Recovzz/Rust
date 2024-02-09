92bbb928dbf7bbedd98fda814b8eb763489afa63
Recovz <93311330+Recovzz@users.noreply.github.com>
2024-02-09 10:16:51 +0100

    Update tests4.rs

--- exercices/17_tests/tests4.rs
+++ exercices/17_tests/tests4.rs
@@ -9,16 +9,17 @@
 
 struct Rectangle {
     width: i32,
-    height: i32
+    height: i32,
 }
 
 impl Rectangle {
-    // Only change the test functions themselves
+    // Implémenter une méthode new pour créer une nouvelle instance de Rectangle
     pub fn new(width: i32, height: i32) -> Self {
+        // Vérifier si les dimensions sont positives
         if width <= 0 || height <= 0 {
             panic!("Rectangle width and height cannot be negative!")
         }
-        Rectangle {width, height}
+        Rectangle { width, height }
     }
 }
 
@@ -28,21 +29,25 @@ mod tests {
 
     #[test]
     fn correct_width_and_height() {
-        // This test should check if the rectangle is the size that we pass into its constructor
+        // Créer une instance de Rectangle avec des dimensions spécifiques
         let rect = Rectangle::new(10, 20);
-        assert_eq!(???, 10); // check width
-        assert_eq!(???, 20); // check height
+        // Vérifier si la largeur est correcte en utilisant l'assertion assert_eq!
+        assert_eq!(rect.width, 10);
+        // Vérifier si la hauteur est correcte en utilisant l'assertion assert_eq!
+        assert_eq!(rect.height, 20);
     }
 
     #[test]
+    #[should_panic]
     fn negative_width() {
-        // This test should check if program panics when we try to create rectangle with negative width
+        // Vérifier si le programme panique lors de la création d'un Rectangle avec une largeur négative
         let _rect = Rectangle::new(-10, 10);
     }
 
     #[test]
+    #[should_panic]
     fn negative_height() {
-        // This test should check if program panics when we try to create rectangle with negative height
+        // Vérifier si le programme panique lors de la création d'un Rectangle avec une hauteur négative
         let _rect = Rectangle::new(10, -10);
     }
 }
